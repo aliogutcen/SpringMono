03317a5bcff674bdbe232cc250310e9cfc0acf2a
aliogutcen <aliogutcen@gmail.com>
2023-02-15 14:36:29 +0300

    Initial commit

--- src/main/java/com/ogutcenali/SpringMono/controller/UserController.java
+++ src/main/java/com/ogutcenali/SpringMono/controller/UserController.java
@@ -3,7 +3,6 @@ package com.ogutcenali.SpringMono.controller;
 import com.ogutcenali.SpringMono.dto.request.LoginRequestDto;
 import com.ogutcenali.SpringMono.dto.request.RegisterRequestDto;
 import com.ogutcenali.SpringMono.dto.response.UserControllerFindAllResponseDto;
-import com.ogutcenali.SpringMono.repository.entity.User;
 import com.ogutcenali.SpringMono.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +23,10 @@ public class UserController {
 
 
     @GetMapping(REGISTER)
-    public ResponseEntity<String> register(RegisterRequestDto dto){
-        userService.save(User.builder().email(dto.getEmail()).username(dto.getUsername()).password(dto.getPassword())
-                .build());
+    public ResponseEntity<Boolean> register(RegisterRequestDto dto){
+       userService.register(dto);
 
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.ok(userService.register(dto));
     }
 
     public ResponseEntity<Boolean> doLogin (LoginRequestDto dto){
--- src/main/java/com/ogutcenali/SpringMono/service/UserService.java
+++ src/main/java/com/ogutcenali/SpringMono/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ogutcenali.SpringMono.service;
 
+import com.ogutcenali.SpringMono.dto.request.RegisterRequestDto;
 import com.ogutcenali.SpringMono.repository.IUserRepository;
 import com.ogutcenali.SpringMono.repository.entity.User;
 import com.ogutcenali.SpringMono.utility.ServiceManager;
@@ -13,5 +14,12 @@ public class UserService extends ServiceManager<User,Long> {
     public UserService(IUserRepository userRepository){
         super(userRepository);
         this.userRepository=userRepository;
+    }
+
+
+    public Boolean  register(RegisterRequestDto dto){
+   User user=     User.builder().password(dto.getPassword()).username(dto.getUsername()).email(dto.getEmail()).build();
+        save(user);
+        return true;
     }
 }
